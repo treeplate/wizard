08c47ae82ad15214485c6b22f425d37df6c6c596
treeplate <tree.plate.moo@gmail.com>
2025-10-10 16:35:28 -0700

    create TASsing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
+import 'package:file_picker/file_picker.dart';
 
 import 'physics.dart';
 
@@ -18,73 +21,159 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   World? world;
   String? file;
-  String filename = 'level1.txt';
+  String filename = 'level1.lvl';
   late final Ticker ticker;
   final FocusNode focusNode = FocusNode();
   Object? player;
   int jumpVel = 20;
   int speed = 5;
   Map<String, Set<Object>> teleportedObjects = {};
+  bool readTas = true;
+  bool writeTas = true;
+  List<String>? tas;
+  int tick = 0;
+
   @override
   void initState() {
     super.initState();
-    ticker = createTicker((Duration duration) {
-      setState(() {
-        world?.tick();
-        if (!(player?.tags.contains('player') ?? true)) {
-          restart();
-        }
-        if (world != null) {
-          Set<Object> deadObjects = {};
-          for (Object obj in teleportedObjects[filename] ?? []) {
-            if (world!.colliders(obj).isEmpty) {
-              world!.objects.add(obj);
-              deadObjects.add(obj);
-            }
-          }
-          teleportedObjects[filename]?.removeAll(deadObjects);
-        }
-        outer:
-        for (Object a in world?.objects ?? []) {
-          for (Object b in world!.objects) {
-            if (a == b) continue;
-            a.y--;
-            if (!world!.colliding(a, b)) {
-              a.y++;
-              continue;
-            }
-            a.y++;
-            for (String tag in b.tags.where((e) => e.startsWith('goto='))) {
-              String nextLevel = tag.substring(5);
-              (teleportedObjects[nextLevel] ??= {}).add(a);
-              a.x = 0;
-              a.y = 0;
-              if (a.tags.contains('player')) {
-                player!.height = 50;
-                player!.tags.add('player');
-                world = null;
-                filename = nextLevel;
-                rootBundle.loadString('levels/$nextLevel').then((
-                  final String file,
-                ) {
-                  setState(() {
-                    world = World.parse(file);
-                    this.file = file;
-                  });
-                });
-                break outer;
-              }
-            }
-          }
-        }
-      });
-    })..start();
+    ticker = createTicker(doTick);
+    if (!writeTas) ticker.start();
     rootBundle.loadString('levels/level1.lvl').then((final String file) {
       setState(() {
         world = World.parse(file);
         this.file = file;
         player = world!.objects.singleWhere((e) => e.tags.contains('player'));
       });
+    });
+    if (readTas) {
+      rootBundle.loadString('tas/$filename.tas').then((final String file) {
+        tas = file.split('\n');
+      });
+    } else if (writeTas) {
+      tas = [];
+    }
+  }
+
+  void tickTo(int goal) {
+    assert(!readTas || tas != null);
+    while (tick < goal) {
+      doTick(Duration(milliseconds: 1000 ~/ 60));
+    }
+  }
+
+  void doTick(Duration duration) {
+    setState(() {
+      if (readTas && tas == null) {
+        return;
+      }
+      if (tas != null) {
+        if (tick < tas!.length && tas![tick] != '') {
+          List<String> events = tas![tick].split('');
+          for (String event in events) {
+            switch (event) {
+              case 'r':
+                if (rightPressed) {
+                  rightUp();
+                } else {
+                  rightDown();
+                }
+              case 'l':
+                if (leftPressed) {
+                  leftUp();
+                } else {
+                  leftDown();
+                }
+              case 'j':
+                jump();
+              case '1':
+                fire();
+              case 'R':
+                restart();
+            }
+          }
+        }
+      }
+      world?.tick();
+      tick++;
+      jumped = false;
+      if (!(player?.tags.contains('player') ?? true)) {
+        restart();
+      }
+      if (world != null) {
+        Set<Object> deadObjects = {};
+        for (Object obj in teleportedObjects[filename] ?? []) {
+          if (world!.colliders(obj).isEmpty) {
+            world!.objects.add(obj);
+            deadObjects.add(obj);
+          }
+        }
+        teleportedObjects[filename]?.removeAll(deadObjects);
+      }
+      outer:
+      for (Object a in world?.objects ?? []) {
+        for (Object b in world!.objects) {
+          if (a == b) continue;
+          a.y--;
+          if (!world!.colliding(a, b)) {
+            a.y++;
+            continue;
+          }
+          a.y++;
+          for (String tag in b.tags.where((e) => e.startsWith('goto='))) {
+            String nextLevel = tag.substring(5);
+            (teleportedObjects[nextLevel] ??= {}).add(a);
+            a.x = 0;
+            a.y = 0;
+            if (a.tags.contains('player')) {
+              player!.height = 50;
+              player!.tags.add('player');
+              world = null;
+              if (writeTas) {
+                FilePickerMacOS()
+                    .pickFiles(
+                      dialogTitle: 'Save TAS to:',
+                      type: FileType.custom,
+                      allowedExtensions: ['tas'],
+                    )
+                    .then((FilePickerResult? result) {
+                      if (result != null) {
+                        File(
+                          result.files.first.path!,
+                        ).writeAsStringSync(tas!.join('\n'));
+                      }
+                    });
+              }
+              tick = 0;
+              filename = nextLevel;
+              if (readTas) {
+                rootBundle
+                    .loadString('tas/$filename.tas')
+                    .then((final String file) {
+                      tas = file.split('\n');
+                    })
+                    .onError((e, st) {
+                      if (writeTas) {
+                        tas = [];
+                      } else {
+                        throw e!;
+                      }
+                    });
+              } else if (writeTas) {
+                tas = [];
+              }
+              rootBundle.loadString('levels/$nextLevel').then((
+                final String file,
+              ) {
+                setState(() {
+                  world = World.parse(file);
+                  this.file = file;
+                });
+              });
+              break outer;
+            }
+          }
+        }
+      }
     });
   }
 
@@ -95,6 +184,9 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   }
 
   KeyEventResult onKeyEvent(FocusNode node, KeyEvent event) {
+    if (textField1FocusNode.hasFocus || textField2FocusNode.hasFocus) {
+      return KeyEventResult.ignored;
+    }
     if (event is KeyRepeatEvent) return KeyEventResult.handled;
     if (event is KeyDownEvent) {
       if (event.logicalKey == LogicalKeyboardKey.keyR) {
@@ -143,6 +235,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   bool leftPressed = false;
   bool rightPressed = false;
+  bool jumped = false;
 
   void leftUp() {
     if (leftPressed) {
@@ -189,11 +282,14 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   }
 
   void jump() {
-    player!.y--;
-    if (world!.colliders(player!).isNotEmpty) {
-      player!.baseYvel += jumpVel;
+    if (!jumped) {
+      player!.y--;
+      if (world!.colliders(player!).isNotEmpty) {
+        player!.baseYvel += jumpVel;
+      }
+      player!.y++;
+      jumped = true;
     }
-    player!.y++;
   }
 
   void fire() {
@@ -211,6 +307,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   }
 
   void restart() {
+    tick = 0;
     world = World.parse(file!);
     player!.x = 0;
     player!.y = 0;
@@ -220,6 +317,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     (teleportedObjects[filename] ??= {}).add(player!);
   }
 
+  FocusNode textField1FocusNode = FocusNode();
+  FocusNode textField2FocusNode = FocusNode();
+  TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     if (world == null) {
@@ -228,41 +329,86 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     return Focus(
       autofocus: true,
       onKeyEvent: onKeyEvent,
-      child: WorldRenderer(world: world),
-    );
-  }
-}
-
-class WorldRenderer extends StatelessWidget {
-  const WorldRenderer({super.key, required this.world});
-
-  final World? world;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(world!.name)),
-      body: Center(
-        child: Container(
-          width: world!.width.toDouble(),
-          height: world!.height.toDouble(),
-          decoration: BoxDecoration(border: Border.all(color: Colors.white)),
-          child: Stack(
+      child: Scaffold(
+        appBar: AppBar(title: Text(world!.name)),
+        body: Center(
+          child: Row(
             children: [
-              ...world!.objects.map(
-                (e) => Positioned(
-                  bottom: e.y,
-                  left: e.x,
-                  child: Container(
-                    color: e.tags.any((e) => e.startsWith('goto='))
-                        ? Colors.green
-                        : e.tags.contains('enemy') || e.tags.contains('fire')
-                        ? Colors.red
-                        : Colors.white,
-                    width: e.width,
-                    height: e.height,
-                  ),
+              Expanded(child: SizedBox()),
+              Container(
+                width: world!.width.toDouble(),
+                height: world!.height.toDouble(),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
                 ),
+                child: Stack(
+                  children: [
+                    ...world!.objects.map(
+                      (e) => Positioned(
+                        bottom: e.y,
+                        left: e.x,
+                        child: Container(
+                          color: e.tags.any((e) => e.startsWith('goto='))
+                              ? Colors.green
+                              : e.tags.contains('enemy') ||
+                                    e.tags.contains('fire')
+                              ? Colors.red
+                              : Colors.white,
+                          width: e.width,
+                          height: e.height,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: writeTas
+                    ? Column(
+                        children: [
+                          Text('Tick $tick'),
+                          TextField(
+                            focusNode: textField1FocusNode,
+                            controller: TextEditingController(
+                              text: tick < tas!.length ? tas![tick] : '',
+                            ),
+                            onChanged: (value) {
+                              while (tick >= tas!.length) {
+                                tas!.add('');
+                              }
+                              tas![tick] = value;
+                            },
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              doTick(Duration.zero);
+                            },
+                            child: Text('Run'),
+                          ),
+                          TextField(
+                            focusNode: textField2FocusNode,
+                            controller: textEditingController,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              int? goal = int.tryParse(
+                                textEditingController.text,
+                              );
+                              if (goal == null) return;
+                              if (goal < tick) {
+                                restart();
+                                player!.moveXvel = 0;
+                                player!.moveYvel = 0;
+                                player!.baseXvel = 0;
+                                player!.baseYvel = 0;
+                              }
+                              tickTo(goal);
+                            },
+                            child: Text('Go to tick'),
+                          ),
+                        ],
+                      )
+                    : SizedBox(),
               ),
             ],
           ),
